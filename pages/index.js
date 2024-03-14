d4dba7bc3b6abaf43c7b979fcf3dc5bf88d8ebf7
Thi <thiago.andradecontato@hotmail.com>
2024-03-14 22:05:22 +0100

    adding files to git

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Builder from '../components/Builder';
+
+const Home = () => {
+  // Estado para controlar se o loading está ativo ou não
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Simulando a conclusão do loading (isso deve ser feito com base no estado do componente LoadingScreen)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Assumindo que o loading dura 2 segundos
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <Builder/>  
+    </>
+  );
+};
+
+export default Home;
